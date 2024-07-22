56b48185f42390250e8dc4c57260db3b5358e03f
Yun NaJa <151639314+Kasidet1@users.noreply.github.com>
2024-07-22 18:44:29 +0700

    Update pin_input_screen.dart

--- pin_input_screen.dart
+++ pin_input_screen.dart
@@ -1,34 +1,177 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PinInputScreen extends StatelessWidget {
+class PinInputScreen extends StatefulWidget {
+  const PinInputScreen({super.key});
+
+  @override
+  State<PinInputScreen> createState() => _PinCodeWidgetState();
+}
+
+class _PinCodeWidgetState extends State<PinInputScreen> {
+  String enteredPin = '';
+  bool isPinVisible = false;
+
+  /// This widget will be used for each digit button
+  Widget numButton(int number) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            if (enteredPin.length < 6) {
+              enteredPin += number.toString();
+              if (enteredPin.length == 6) {
+                // Navigate to HomeScreen
+                Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
+              }
+            }
+          });
+        },
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(16),
+          backgroundColor: Colors.blueGrey, // Background color
+          foregroundColor: Colors.white,   // Text color
+          minimumSize: Size(60, 60), // Button size
+        ),
+        child: Text(
+          number.toString(),
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          physics: const BouncingScrollPhysics(),
           children: [
-            Text('Please enter your PIN'),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: 3,
-                childAspectRatio: 2,
-                children: List.generate(12, (index) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      // Handle PIN input here
-                    },
-                    child: Text(index == 9
-                        ? ''
-                        : index == 10
-                            ? '0'
-                            : index == 11
-                                ? '⌫'
-                                : (index + 1).toString()),
+            const Center(
+              child: Text(
+                'Enter Your Pin',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+
+            /// Pin code area
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                6,
+                (index) {
+                  return Container(
+                    margin: const EdgeInsets.all(6.0),
+                    width: isPinVisible ? 50 : 16,
+                    height: isPinVisible ? 50 : 16,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6.0),
+                      color: index < enteredPin.length
+                          ? isPinVisible
+                              ? Colors.green
+                              : CupertinoColors.activeBlue
+                          : CupertinoColors.activeBlue.withOpacity(0.1),
+                    ),
+                    child: isPinVisible && index < enteredPin.length
+                        ? Center(
+                            child: Text(
+                              enteredPin[index],
+                              style: const TextStyle(
+                                fontSize: 17,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          )
+                        : null,
                   );
-                }),
+                },
+              ),
+            ),
+
+            /// Visibility toggle button
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isPinVisible = !isPinVisible;
+                });
+              },
+              icon: Icon(
+                isPinVisible ? Icons.visibility_off : Icons.visibility,
+              ),
+            ),
+
+            SizedBox(height: isPinVisible ? 50.0 : 8.0),
+
+            /// Digits
+            for (var i = 0; i < 3; i++)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(
+                    3,
+                    (index) => numButton(1 + 3 * i + index),
+                  ).toList(),
+                ),
+              ),
+
+            /// 0 digit with backspace button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(child: Container()), // Empty space before the 0 button
+                  Center(child: numButton(0)), // Centered 0 button
+                  Expanded(child: Container()), // Empty space after the 0 button
+                  TextButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          if (enteredPin.isNotEmpty) {
+                            enteredPin =
+                                enteredPin.substring(0, enteredPin.length - 1);
+                          }
+                        },
+                      );
+                    },
+                    child: const Icon(
+                      Icons.backspace,
+                      color: Colors.black,
+                      size: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            /// Reset button
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  enteredPin = '';
+                });
+              },
+              child: const Text(
+                'Reset',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
               ),
             ),
           ],
